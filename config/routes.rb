33067b2593c2498297feb38ef816f8646b7ed7a8
Libranner <libranner@gmail.com>
2013-07-27 16:12:16 -0400

    Routes file including route to get models, makes and year of a vehicle

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Hitchhiker::Application.routes.draw do
+  resources :vehicles, only:[:index, :create]
+  get 'vehicles/makes'
+  get "vehicles/:makes/models", to: 'vehicles#models'
+  get "vehicles/:makes/:models/years", to: 'vehicles#years'
+
   authenticated :user do
     root :to => 'home#index'
   end
+  match 'auth/:provider/callback', to: 'authentications#create'
+  match 'auth/failure', to: redirect('/')
+
+
   root :to => "home#index"
-  devise_for :users
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+             #,controllers: {omniauth_callbacks: "authentications"}
   resources :users
 end
